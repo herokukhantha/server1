5854e488cdb12f0cfe85330ef964a597a548f258
herokukhantha <heroku.khantha@gmail.com>
2023-01-27 09:15:57 +0700

    first comm975

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express()
 
 app.get("/", (req,res) =>{
-    res.send("API is running.1..");
+    res.send("API is running.2.");
 })
 
 app.listen(5000, console.log("server running1"));
